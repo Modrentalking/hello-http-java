1eed68b1d36e96c8cd0d38ef7821975683b19f61
modest <maximodest@gmail.com>
2024-06-07 20:18:37 +0300

    TEST

--- HelloWorld.java
+++ HelloWorld.java
@@ -28,7 +28,7 @@ public class HelloWorld {
 	    System.out.println("Request received from: " + t.getRemoteAddress().toString());
 
 	    // Displaying Hello message
-	    String hello = "Hello TMS !!";
+	    String hello = "Hello TMS and ME !!";
 	    String lang = env.get("HELLO_LANG");
 	    if (lang != null && lang.equals("FR")) {
 		hello = "Bonjour Criteo !!";
